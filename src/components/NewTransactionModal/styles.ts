6f476a8e398adf6fd49ce154d6474227a2de0b64
SrLabres <josue.labres.cruz@gmail.com>
2021-07-25 11:26:14 -0300

    cores dos botões

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { darken } from 'polished';
+import { darken, transparentize } from 'polished';
 
 export const Container = styled.form `
 
@@ -58,17 +58,33 @@ export const TransactionTypeContainer = styled.div `
     grid-template-columns: 1fr 1fr;
     gap: 0.5rem;
 
-    button {
+`
+
+interface RadioBoxProps {
+    isActive: boolean,
+    activeColor: 'green' | 'red'
+}
+
+const colors = {
+       green: '#33CC95'  ,
+       red: '#E52E4D'   
+}
+
+
+export const RadioBox = styled.button<RadioBoxProps> `
+
+
         height: 4rem;
         border: 1px solid #d7d7d7;
         border-radius: 0.25rem;
 
-        background: transparent;
+        background: ${(props) => props.isActive ? transparentize(0.9, colors[props.activeColor]) : 'transparent'};
 
         display: flex;
         align-items: center;
         justify-content: center;
         transition: border-color 0.2s;
+        transition: background 0.3s;
 
         &:hover {
             border-color: ${darken(0.1, '#d7d7d7')};
@@ -86,6 +102,4 @@ export const TransactionTypeContainer = styled.div `
             font-size: 1rem;
             color: var(--text-title)
         }
-    }
-
 `
